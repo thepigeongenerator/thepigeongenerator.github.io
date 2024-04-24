40f10a91e618c5aba3aae36fac511f765b509ee6
Quinn <99677023+thepigeongenerator@users.noreply.github.com>
2024-04-24 12:50:19 +0200

    made base website

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 //entry-point of the application
 function main() {
+    // 1% to randomly load the page in light mode. just because ¯\_(ツ)_/¯
+    if (Math.random() < 0.01) {
+        const bodyElmt = document.getElementsByTagName("body")[0];
+        bodyElmt.setAttribute("data-bs-theme", "light");
+    }
+    
+    //execute qeffects
     execute_effects();
 }
 
