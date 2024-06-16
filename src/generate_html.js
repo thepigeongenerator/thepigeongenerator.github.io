27107c7df214f545fbce97f5d9533f0cb5168797
Quinn <99677023+thepigeongenerator@users.noreply.github.com>
2024-06-16 20:10:41 +0200

    fix projects not showing #4. no jquery

--- src/generate_html.js
+++ src/generate_html.js
@@ -7,35 +7,37 @@ function create_element_with_classes(element, classes) {
 }
 
 function generate_cards(id, json_path) {
-    $.getJSON(json_path, function (card_data) {
-        const elmt = document.getElementById(id);
+    fetch(json_path)
+        .then(response => response.json())
+        .then(card_data => {
+            const elmt = document.getElementById(id);
 
-        for (let i = 0; i < card_data.length; i++) {
-            const card = create_element_with_classes("div", ["card", "m-3"]);
-            card.style.width = "18rem";
+            for (let i = 0; i < card_data.length; i++) {
+                const card = create_element_with_classes("div", ["card", "m-3"]);
+                card.style.width = "18rem";
 
-            const image = create_element_with_classes("img", ["card-img-top"]);
-            image.src = card_data[i].image;
-            image.alt = `${card_data[i].title} image`;
+                const image = create_element_with_classes("img", ["card-img-top"]);
+                image.src = card_data[i].image;
+                image.alt = `${card_data[i].title} image`;
 
-            const card_body = create_element_with_classes("div", ["card-body"]);
-            const card_title = create_element_with_classes("h5", ["card-title"]);
-            card_title.innerText = card_data[i].title;
+                const card_body = create_element_with_classes("div", ["card-body"]);
+                const card_title = create_element_with_classes("h5", ["card-title"]);
+                card_title.innerText = card_data[i].title;
 
-            const card_text = create_element_with_classes("p", ["card-text"]);
-            card_text.innerText = card_data[i].body;
+                const card_text = create_element_with_classes("p", ["card-text"]);
+                card_text.innerText = card_data[i].body;
 
-            const link = create_element_with_classes("a", ["btn", "btn-primary"]);
-            link.innerText = card_data[i].link.text;
-            link.href = card_data[i].link.href;
+                const link = create_element_with_classes("a", ["btn", "btn-primary"]);
+                link.innerText = card_data[i].link.text;
+                link.href = card_data[i].link.href;
 
-            //append the elements
-            elmt.appendChild(card);
-            card.appendChild(image);
-            card.appendChild(card_body);
-            card_body.appendChild(card_title);
-            card_body.appendChild(card_text);
-            card_body.appendChild(link);
-        }
-    });
+                //append the elements
+                elmt.appendChild(card);
+                card.appendChild(image);
+                card.appendChild(card_body);
+                card_body.appendChild(card_title);
+                card_body.appendChild(card_text);
+                card_body.appendChild(link);
+            }
+        });
 }
