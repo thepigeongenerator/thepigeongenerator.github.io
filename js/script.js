1d9f27f819b8af65d6a0b7b4b0485f2346510f0c
thepigeongenerator <thepigeongenerator@users.noreply.github.com>
2024-09-21 15:22:46 +0000

    deploy: 6d6a5490639ec21c6970b7dd30f325f4a883f6a9

--- js/script.js
+++ js/script.js
@@ -1,37 +1,124 @@
 "use strict";
-// replace the args in data, return result
+function str_insert_at(str, index, insert) {
+    return str.substring(0, index) + insert + str.substring(index + 1);
+}
+class Colour {
+    constructor(packed) {
+        this.packed = packed;
+    }
+    get r() { return (this.packed & 0xFF000000) >>> 24; }
+    get g() { return (this.packed & 0x00FF0000) >>> 16; }
+    get b() { return (this.packed & 0x0000FF00) >>> 8; }
+    get a() { return (this.packed & 0x000000FF); }
+    get packedvalue() { return this.packed; }
+    static fromRGBA(r, g, b, a = 0xFF) {
+        return new Colour(((r << 24) | (g << 16) | (b << 8) | a) >>> 0);
+    }
+    static fromCSS(value) {
+        if (value.startsWith("var")) {
+            const varname = value.replace(/[()]|(var)/g, "");
+            const root = document.querySelector(':root');
+            if (root === null)
+                return new Colour(0);
+            const style = getComputedStyle(root);
+            value = style.getPropertyValue(varname);
+        }
+        let str = value.replace("#", "");
+        if (str.length === 6)
+            str += "FF";
+        const packed = parseInt(str, 16) >>> 0;
+        return new Colour(packed);
+    }
+    static lerp(a, b, t) {
+        return Colour.fromRGBA(Math.floor(a.r + (b.r - a.r) * t), Math.floor(a.g + (b.g - a.g) * t), Math.floor(a.b + (b.b - a.b) * t), Math.floor(a.a + (b.a - a.a) * t));
+    }
+    toString() {
+        return "#" + this.packed.toString(16).padStart(8, '0');
+    }
+}
+function effect_typewriter(elmt) {
+    const letterDelay = 30;
+    const randomRepeatCount = 3;
+    const text = elmt.innerText;
+    elmt.innerText = Array(text.length).fill('\xa0').join('');
+    const loop_characters = (i = 0) => {
+        const loop_random_repeat = (j = 0) => {
+            const charCode = Math.floor(Math.random() * (0x7F - 0x21) + 0x21);
+            const char = String.fromCharCode(charCode);
+            elmt.innerText = str_insert_at(elmt.innerText, i, char);
+            if (j < randomRepeatCount) {
+                setTimeout(() => loop_random_repeat(j + 1), letterDelay);
+            }
+            else {
+                setTimeout(() => {
+                    elmt.innerText = str_insert_at(elmt.innerText, i, text.substring(i, i + 1));
+                    setTimeout(() => loop_characters(i + 1), letterDelay);
+                }, letterDelay);
+            }
+        };
+        if (i < text.length) {
+            setTimeout(loop_random_repeat, letterDelay);
+        }
+    };
+    loop_characters();
+}
+function effect_radialgradient(elmt) {
+    var _a, _b;
+    const foreground = Colour.fromCSS((_a = elmt.dataset.foreground) !== null && _a !== void 0 ? _a : "#00000000");
+    const background = Colour.fromCSS((_b = elmt.dataset.background) !== null && _b !== void 0 ? _b : "#00000000");
+    const speed = elmt.dataset.speed !== undefined ? parseInt(elmt.dataset.speed) : 1;
+    let currForeground = background;
+    elmt.style.backgroundAttachment = "fixed";
+    const lerp_colours = () => {
+        const set_style = () => elmt.style.background = `radial-gradient(${currForeground.toString()}, ${background.toString()})`;
+        set_style();
+        currForeground = Colour.lerp(currForeground, foreground, 0.01);
+        if (foreground.packedvalue !== background.packedvalue)
+            setTimeout(lerp_colours, speed);
+        else
+            set_style();
+    };
+    lerp_colours();
+}
+function execute_effects(elmt) {
+    const effects = {
+        typewriter: effect_typewriter,
+        radialgradient: effect_radialgradient
+    };
+    for (let effect in effects) {
+        const elements = (elmt === null) ?
+            document.querySelectorAll(".qeffect-" + effect) :
+            elmt.querySelectorAll(".qeffect-" + effect);
+        for (let i = 0; i < elements.length; i++) {
+            effects[effect](elements[i]);
+        }
+    }
+}
 function parse_data(data, args) {
     for (let i = 0; i < args.length; i++) {
-        // replace the index surrounded by curly braces {0}, {1}, etc with the value in args
         data = data.replace("{" + i + "}", args[i]);
     }
     return data;
 }
-// loads html from an external file
 function load_html() {
-    // get all elements with the html tag to load in
     const elmts = document.querySelectorAll(".load-html");
-    // loop through the elements
     for (let i = 0; i < elmts.length; i++) {
         const elmt = elmts[i];
-        const file_path = elmt.dataset.filepath; // extract the file path from the dataset
+        const file_path = elmt.dataset.filepath;
         if (file_path == undefined) {
             console.error(`file_path was not set on element ${elmt}`);
             return;
         }
-        // get the file at the file path
-        fetch(file_path).then(response => response.text()) // get text
+        fetch(file_path).then(response => response.text())
             .then(data => {
-            // parse the html using the arguments if args is defined
             if (elmt.dataset.args !== undefined) {
-                data = parse_data(data, elmt.dataset.args.split(",")); // get the arguments seperated by ,
+                data = parse_data(data, elmt.dataset.args.split(","));
             }
-            elmt.innerHTML += data; // write the responce in the innerHtml
+            elmt.innerHTML += data;
             execute_effects(elmt);
         });
     }
 }
-// creates an element with classess
 function create_element_with_classes(element, classes) {
     const elmt = document.createElement(element);
     for (let i = 0; i < classes.length; i++) {
@@ -39,12 +126,10 @@ function create_element_with_classes(element, classes) {
     }
     return elmt;
 }
-// generates cards (eg for projects)
 function generate_cards(elmt, json_path) {
-    fetch(json_path) // get the file at the json path
-        .then(response => response.json()) // get the response in json format
+    fetch(json_path)
+        .then(response => response.json())
         .then(card_data => {
-        // create cards
         for (let i = 0; i < card_data.length; i++) {
             const card = create_element_with_classes("div", ["card"]);
             const image = create_element_with_classes("img", []);
@@ -58,14 +143,13 @@ function generate_cards(elmt, json_path) {
             const link_container = create_element_with_classes("div", ["card-links"]);
             const links = [];
             for (let j = 0; j < card_data[i].links.length; j++) {
-                const link = create_element_with_classes("a", ["btn", "btn-sm", "btn-outline-primary", "m-1"]);
+                const link = create_element_with_classes("a", ["button"]);
                 link.innerText = card_data[i].links[j].text;
                 link.href = card_data[i].links[j].href;
-                link.target = "_blank"; // make the link open in a new tab
+                link.target = "_blank";
                 link.rel = "noopener noreferrer";
-                links.push(link); // add to the end of the array
+                links.push(link);
             }
-            //append the elements
             elmt.appendChild(card);
             card.appendChild(image);
             card.appendChild(card_body);
@@ -77,13 +161,9 @@ function generate_cards(elmt, json_path) {
         }
     });
 }
-//entry-point of the application
 function main() {
-    //execute qeffects
     execute_effects(null);
-    // loads html (will call qeffects itself)
     load_html();
-    // loads cards
     const elmts = document.querySelectorAll(".generate-cards");
     for (let i = 0; i < elmts.length; i++) {
         const elmt = elmts[i];
@@ -95,66 +175,3 @@ function main() {
     }
 }
 main();
-//writes the string out, but first writes random characters.
-function effect_typewriter(elmt) {
-    const letterDelay = 30;
-    const randomRepeatCount = 3;
-    const text = elmt.innerText;
-    elmt.innerText = Array(text.length).fill('\xa0').join(''); //set the text to spaces with the same length
-    //loop through the characters and print a random one a few times before writing the correct one
-    const loop_characters = (i = 0) => {
-        //get a random (visible) character and set it at the current string position
-        const loop_random_repeat = (j = 0) => {
-            const charCode = Math.floor(Math.random() * (0x7F - 0x21) + 0x21);
-            const char = String.fromCharCode(charCode);
-            elmt.innerText = str_insert_at(elmt.innerText, i, char); //set the character at position i
-            if (j < randomRepeatCount) {
-                setTimeout(() => loop_random_repeat(j + 1), letterDelay);
-            }
-            else {
-                setTimeout(() => {
-                    elmt.innerText = str_insert_at(elmt.innerText, i, text.substring(i, i + 1)); //set the correct character
-                    setTimeout(() => loop_characters(i + 1), letterDelay);
-                }, letterDelay);
-            }
-        };
-        if (i < text.length) {
-            setTimeout(loop_random_repeat, letterDelay);
-        }
-    };
-    //start the loop
-    loop_characters();
-}
-//slowly fades in a linear gradient
-function effect_radialgradient(elmt) {
-    const speed = 255 * 20;
-    for (let i = 0; i < 0xFF; i++) {
-        setTimeout(() => {
-            elmt.style.background = `radial-gradient(rgb(${i}, 0, ${i}), #000000)`;
-            elmt.style.backgroundAttachment = "fixed"; //prevent the background from scrolling
-        }, (speed / 0xFF) * i);
-    }
-}
-//finds the elements with the different class names and execute the correct effect on them
-function execute_effects(elmt) {
-    // define the effects that exist
-    const effects = {
-        typewriter: effect_typewriter,
-        radialgradient: effect_radialgradient
-    };
-    //apply effects
-    for (let effect in effects) {
-        //find the elements with the current effect
-        const elements = (elmt === null) ?
-            document.querySelectorAll(".qeffect-" + effect) :
-            elmt.querySelectorAll(".qeffect-" + effect);
-        //execute the associated effect function on these elements
-        for (let i = 0; i < elements.length; i++) {
-            effects[effect](elements[i]);
-        }
-    }
-}
-//inserts a string at a location in a string, returns the new string
-function str_insert_at(str, index, insert) {
-    return str.substring(0, index) + insert + str.substring(index + 1);
-}
