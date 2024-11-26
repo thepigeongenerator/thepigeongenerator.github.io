c9534677c67ba6e1d2248f269fb3f6bf0d26fd9e
thepigeongenerator <thepigeongenerator@users.noreply.github.com>
2024-11-26 14:31:49 +0000

    deploy: 3f899b36beb5f12507d8eb1e68a0297119b72880

--- js/script.js
+++ js/script.js
@@ -94,76 +94,8 @@ function execute_effects(elmt) {
         }
     }
 }
-function parse_data(data, args) {
-    for (let i = 0; i < args.length; i++) {
-        data = data.replace("{" + i + "}", args[i]);
-    }
-    return data;
-}
-function load_html() {
-    const elmts = document.querySelectorAll(".load-html");
-    for (let i = 0; i < elmts.length; i++) {
-        const elmt = elmts[i];
-        const file_path = elmt.dataset.filepath;
-        if (file_path == undefined) {
-            console.error(`file_path was not set on element ${elmt}`);
-            return;
-        }
-        fetch(file_path).then(response => response.text())
-            .then(data => {
-            if (elmt.dataset.args !== undefined) {
-                data = parse_data(data, elmt.dataset.args.split(","));
-            }
-            elmt.innerHTML += data;
-            execute_effects(elmt);
-        });
-    }
-}
-function create_element_with_classes(element, classes) {
-    const elmt = document.createElement(element);
-    for (let i = 0; i < classes.length; i++) {
-        elmt.classList.toggle(classes[i]);
-    }
-    return elmt;
-}
-function generate_cards(elmt, json_path) {
-    fetch(json_path)
-        .then(response => response.json())
-        .then(card_data => {
-        for (let i = 0; i < card_data.length; i++) {
-            const card = create_element_with_classes("div", ["card"]);
-            const image = create_element_with_classes("img", []);
-            image.src = card_data[i].image;
-            image.alt = `${card_data[i].title} image`;
-            const card_body = create_element_with_classes("div", ["card-body"]);
-            const card_title = create_element_with_classes("h5", ["card-title"]);
-            card_title.innerText = card_data[i].title;
-            const card_text = create_element_with_classes("p", ["card-text"]);
-            card_text.innerText = card_data[i].body;
-            const link_container = create_element_with_classes("div", ["card-links"]);
-            const links = [];
-            for (let j = 0; j < card_data[i].links.length; j++) {
-                const link = create_element_with_classes("a", ["button"]);
-                link.innerText = card_data[i].links[j].text;
-                link.href = card_data[i].links[j].href;
-                link.target = "_blank";
-                link.rel = "noopener noreferrer";
-                links.push(link);
-            }
-            elmt.appendChild(card);
-            card.appendChild(image);
-            card.appendChild(card_body);
-            card_body.appendChild(card_title);
-            card_body.appendChild(card_text);
-            card_body.appendChild(link_container);
-            for (let j = 0; j < links.length; j++)
-                link_container.appendChild(links[j]);
-        }
-    });
-}
 function main() {
     execute_effects(null);
-    load_html();
     const elmts = document.querySelectorAll(".generate-cards");
     for (let i = 0; i < elmts.length; i++) {
         const elmt = elmts[i];
@@ -171,7 +103,6 @@ function main() {
             console.error("did not enter a file path to load");
             return;
         }
-        generate_cards(elmt, elmt.dataset.filepath);
     }
 }
 main();
