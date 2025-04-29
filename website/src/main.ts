c8ee436dda6d0c3645275f37c54baf04eb2455c7
Quinn <quinnthepigeon@gmail.com>
2025-04-29 22:09:06 +0200

    enforce new style guide.

--- website/src/main.ts
+++ website/src/main.ts
@@ -1,17 +1,17 @@
 //entry-point of the application
 function main() {
-    //execute qeffects
-    execute_effects(null);
+	//execute qeffects
+	execute_effects(null);
 
-    // loads cards
-    const elmts = document.querySelectorAll(".generate-cards")
-    for (let i = 0; i < elmts.length; i++) {
-        const elmt = elmts[i] as HTMLElement;
-        if (elmt.dataset.filepath === undefined) {
-            console.error("did not enter a file path to load");
-            return;
-        }
-    }
+	// loads cards
+	const elmts = document.querySelectorAll(".generate-cards")
+	for (let i = 0; i < elmts.length; i++) {
+		const elmt = elmts[i] as HTMLElement;
+		if (elmt.dataset.filepath === undefined) {
+			console.error("did not enter a file path to load");
+			return;
+		}
+	}
 }
 
 main();
